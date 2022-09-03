c2f0b258828a9e5e8ab7f9c74d2206dff7fffb83
Mateus Cavalcanti <mateusdcc@proton.me>
2022-09-03 16:58:55 -0300

    Changing footer text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,10 @@ export default function Footer() {
     <footer className="footer items-center bottom-0 p-4 bg-neutral text-neutral-content">
       <div className="items-center grid-flow-col">
         <FaStickyNote />
-        <p>Copyright © 2022 - All right reserved</p>
+        <p>
+          Copyright © 2022 - All right reserved <br /> Made to the
+          &quot;Esquenta Programa de Formação&quot; program of Orange Juice!
+        </p>
       </div>
       <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end mr-6">
         <a href="https://github.com/mateusdcc/anote" target="blank">
